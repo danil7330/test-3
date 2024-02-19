9e7539c9b24886fdc8f0332fc179249ede809fe2
Neto test <danilvot-2011@mail.ru>
2024-02-19 15:41:19 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 12400;
+        int bonus = 10;
+        int result = price / bonus;
+        System.out.println("Количество бонусных миль " + result);
+
+    }
+}
